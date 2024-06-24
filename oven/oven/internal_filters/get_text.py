6f96e2d0ca7615a08d318eddf0da8d920a219dbd
Kamil Tokarski <kamil@tokarski.xyz>
2024-06-24 21:55:00 +0200

    Update to ver 1.0

--- oven/oven/internal_filters/get_text.py
+++ oven/oven/internal_filters/get_text.py
@@ -9,12 +9,8 @@ FILTER_NAME = 'gettext'
 
 def custom_filter(msgid: str, lang: Optional[str] = 'en', should_gather: bool = True, msgstr: Optional[str] = '') -> str:
     trans = Translator()
-
     if trans.config.is_gather_config():
         if should_gather:
-            if Config().get_active_node():
-                trans.add_text(f'{Config().get_active_node()}-{msgid}', msgstr)
-            else:
-                trans.add_text(msgid, msgstr)
+            trans.add_text(msgid, msgstr)
             return ''
-    return trans.get_text(f'{Config().get_active_node()}-{msgid}', lang)
+    return trans.get_text(f'{msgid}', lang)
